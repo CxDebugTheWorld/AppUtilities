a681c1833dc13bbe3ea07dcbac9c13727137c3d7
Jonas Zell <zell.jonas97@gmail.com>
2022-08-15 14:15:23 +0200

    add shapes

---
+++ Sources/AppUtilities/Shapes/RegularPolygon.swift
@@ -0,0 +1,34 @@
+
+import SwiftUI
+
+public struct RegularPolygon: Shape {
+    /// The number of sides of this polygon.
+    let sideCount: Int
+    
+    func path(in rect: CGRect) -> Path {
+        let radius = min(rect.height, rect.width) * 0.5
+        let center = rect.center
+        
+        var path = Path()
+        guard sideCount >= 3 else {
+            Log.general.critical("attempting to draw regular polygon with \(sideCount) sides", submitAnalytics: true)
+            path.addArc(center: center, radius: radius, startAngle: .zero, endAngle: .degrees(360), clockwise: true)
+            
+            return path
+        }
+        
+        let angleStep = 360.0 / Double(sideCount)
+        let firstPt = center + pointOnCircle(radius: radius, angle: .zero)
+        
+        path.move(to: firstPt)
+        
+        for i in 1..<sideCount {
+            let nextPt = pointOnCircle(radius: radius, angle: .init(degrees: Double(i) * angleStep))
+            path.addLine(to: center + nextPt)
+        }
+        
+        path.addLine(to: firstPt)
+        
+        return path
+    }
+}
