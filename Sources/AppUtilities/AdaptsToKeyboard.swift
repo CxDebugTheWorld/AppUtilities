8343a97bad9286dcfb564fb34b5de704eb08e895
Jonas Zell <zell.jonas97@gmail.com>
2022-08-26 11:53:14 +0200

    add AdaptsToKeyboard

---
+++ Sources/AppUtilities/AdaptsToKeyboard.swift
@@ -0,0 +1,41 @@
+
+import SwiftUI
+import Combine
+
+/// Add bottom padding to a view when the system keyboard appears to keep the text field visible.
+/// Inspired by https://stackoverflow.com/a/60178361/7564976
+struct AdaptsToKeyboard: ViewModifier {
+    @State var currentHeight: CGFloat = 0
+    
+    func body(content: Content) -> some View {
+        GeometryReader { geometry in
+            content
+                .padding(.bottom, self.currentHeight)
+                .onAppear(perform: {
+                    NotificationCenter.Publisher(center: NotificationCenter.default, name: UIResponder.keyboardWillShowNotification)
+                        .merge(with: NotificationCenter.Publisher(center: NotificationCenter.default, name: UIResponder.keyboardWillChangeFrameNotification))
+                        .compactMap { notification in
+                            withAnimation(.easeOut(duration: 0.16)) {
+                                notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect
+                            }
+                        }
+                        .map { rect in
+                            rect.height - geometry.safeAreaInsets.bottom
+                        }
+                        .subscribe(Subscribers.Assign(object: self, keyPath: \.currentHeight))
+                    
+                    NotificationCenter.Publisher(center: NotificationCenter.default, name: UIResponder.keyboardWillHideNotification)
+                        .compactMap { notification in
+                            CGFloat.zero
+                        }
+                        .subscribe(Subscribers.Assign(object: self, keyPath: \.currentHeight))
+                })
+        }
+    }
+}
+
+extension View {
+    func adaptsToKeyboard() -> some View {
+        return modifier(AdaptsToKeyboard())
+    }
+}
