1e97c378ac11caa7025d7bcfca087a54c31ecfc4
Jonas Zell <zell.jonas97@gmail.com>
2022-09-01 10:09:34 +0200

    add UIView rendering function

--- Sources/AppUtilities/Image.swift
+++ Sources/AppUtilities/Image.swift
@@ -271,3 +271,12 @@ public extension CGBitmapInfo {
         return alphaInfo == .premultipliedFirst || alphaInfo == .premultipliedLast
     }
 }
+
+public extension UIView {
+    func asImage(rect: CGRect) -> UIImage {
+        let renderer = UIGraphicsImageRenderer(bounds: rect)
+        return renderer.image { rendererContext in
+            layer.render(in: rendererContext.cgContext)
+        }
+    }
+}
