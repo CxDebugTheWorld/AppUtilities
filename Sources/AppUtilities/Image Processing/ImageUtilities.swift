6668b399ed00d949495f38360073b37e2115213d
Jonas Zell <zell.jonas97@gmail.com>
2022-09-07 14:46:58 +0200

    add view snapshotting

--- Sources/AppUtilities/Image Processing/ImageUtilities.swift
+++ Sources/AppUtilities/Image Processing/ImageUtilities.swift
@@ -1,5 +1,6 @@
 
 import UIKit
+import SwiftUI
 
 public extension UIImage {
     convenience init?(pixelBuffer: CVPixelBuffer) {
@@ -382,6 +383,22 @@ public extension CVPixelBuffer {
             let bytesPerRow = CVPixelBufferGetBytesPerRowOfPlane(self, plane)
             
             memcpy(dest, source, height * bytesPerRow)
+        }
+    }
+}
+
+public extension View {
+    func snapshot() -> UIImage {
+        let controller = UIHostingController(rootView: self)
+        let view = controller.view
+        
+        let targetSize = controller.view.intrinsicContentSize
+        view?.bounds = CGRect(origin: .zero, size: targetSize)
+        view?.backgroundColor = .clear
+        
+        let renderer = UIGraphicsImageRenderer(size: targetSize)
+        return renderer.image { _ in
+            view?.drawHierarchy(in: controller.view.bounds, afterScreenUpdates: true)
         }
     }
 }
